d22c9dfdcaf5bead1a2ba8c1e5a878e4f7bcf7b7
pranithreddy <pranithreddy@172.16.80.98>
2018-07-06 17:03:47 +0530

    next commit

--- WebContent/myfirst.js
+++ WebContent/myfirst.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var url = require('url');
-var dt = require('./dateModule')
 var path = require('path')
 var fs = require('fs')
 var express = require('express');
@@ -15,9 +14,18 @@ var mongo_url = "mongodb://localhost:27017/";
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 //app.use(express.bodyParser());
+app.get('/', function(req, res){
+	var filename = "templates/home.html";
+	var filepath = path.join(__dirname, filename);
+	console.log("Accessed home page");
+	fs.readFile(filepath, {encoding: 'utf-8'},function(err, data){
+		res.send(data);
+	});
+	
+});
 
 app.get('/Login', function(req, res) {
-	var filename = "Login.html";
+	var filename = "templates/Login.html";
 	var filepath = path.join(__dirname, filename);
 	console.log("accessed to login page via get");
 	fs.readFile(filepath, {encoding: 'utf-8'}, function(err,data)
@@ -26,7 +34,7 @@ app.get('/Login', function(req, res) {
   	});
 });
 app.get('/profile', function(req, res) {
-	var filename = "profile.html";
+	var filename = "templates/profile.html";
 	var filepath = path.join(__dirname, filename);
 	console.log("accessed to profile via get");
 	fs.readFile(filepath, {encoding: 'utf-8'}, function(err, data)
@@ -35,7 +43,7 @@ app.get('/profile', function(req, res) {
 	});
 });
 app.post('/profile', function(req, res) {
-	var filename = "profile.html";
+	var filename = "templates/profile.html";
 	var filepath = path.join(__dirname, filename);
 	console.log("accessed to profile via post");
 	fs.readFile(filepath, {encoding: 'utf-8'}, function(err, data)
@@ -47,28 +55,25 @@ app.post('/profile', function(req, res) {
 app.post('/profilequery', function(req,res){
 	var q = url.parse(req.url, true).query;
 	var data = req.body;
-	var username = data.name;
-	var password = data.password;
+	var Username = data.name;
+	var Password = data.password;
 	console.log("Querying the login request where")
-	console.log("username :" + username);
-	console.log("password :" + password);
-	var flag=0;
+	console.log("username :" + Username);
+	console.log("password :" + Password);
 	//Accessing the database and querying the data
 	mongo.connect(mongo_url, function(err,db){
 		if (err) throw err;
 		var mdb = db.db("webdb");
-		
-		mdb.collection("userCredentials").find({name : username},{_id : 0, password : 1}, function(err, result){
-			console.log(result);
+		var mquery = {username: Username};
+		mdb.collection("userCredentials").find(mquery).toArray(function(err, result){
 			if(err) throw err;
-			if(password == result.password)
-				flag=1;
+			if(Password === result[0].password)
+				res.send("Log in successful-" + Username);
+			else
+				res.send("Invalid username or password");
 		});
 	});
-	if(flag)
-		res.send("Log in successful-" + username);
-	else
-		res.send("Invalid username or password");
+				
 });
 
 app.listen(5000, function() {
